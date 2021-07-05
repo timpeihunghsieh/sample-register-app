d96712011ba6dff3259a06c5f211c5eb6aad7f71
Tim Hsieh <timpeihunghsieh@gmail.com>
2021-07-05 09:46:54 -0400

    Add /profile endpoint. This is a protected endpoint.

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const passport = require('passport');
 const mongoose = require('mongoose');
 const dbconfig = require('./config/database');
 
@@ -23,6 +24,12 @@ const port = 3000;
 // Body Parser Middleware
 // Parase HTTP Request body to JSON object.
 app.use(bodyParser.json());
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
 
 // Add routes
 app.use('/users', users);
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const config = require('../config/database')
 const User = require('../models/user');
@@ -56,5 +57,12 @@ router.post ('/authenticate', (req, res, next) => {
     });
   });
 });
+
+// Profile
+router.get('/profile', passport.authenticate('jwt', { session: false }),
+  (req, res, next) => {
+    res.json({user: req.user});
+  }
+);
 
 module.exports = router;
